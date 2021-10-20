62fb043f88ed06577f4a5cad08caee08a796914c
cengiz <cengizucak@outlook.com>
2021-10-20 21:13:57 +0200

    20 Ekim Gun Sonu

---
+++ src/app.js
@@ -0,0 +1,28 @@
+console.log('HELLO MONICA 88')
+
+//js type safe degildir
+//const sabit
+
+let bb=9.1;
+
+let dd=9;
+console.log(dd)
+
+const edd=11.2
+console.log(edd)
+
+
+let konutKredileri=["KGK KGK","WWW WWW","SSS SSS","MMM MMM"]
+
+for(let i=0;i<konutKredileri.length;i++){
+    console.log(konutKredileri[i])
+}
+
+console.log(konutKredileri)
+
+console.log("<ul>")
+for(let i=0;i<konutKredileri.length;i++){
+    console.log("<li>"+konutKredileri [i]+"</li>")
+}
+
+console.log("</ul>")

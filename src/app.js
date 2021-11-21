a497a38b40ea0fd83ffbecda171c677b630d0498
cengiz <cengizucak@outlook.com>
2021-11-21 20:20:22 +0100

    21 Kasin Gun Sonu

--- src/app.js
+++ src/app.js
@@ -1,28 +1,10 @@
-console.log('HELLO MONICA 88')
-
-//js type safe degildir
-//const sabit
-
-let bb=9.1;
-
-let dd=9;
-console.log(dd)
-
-const edd=11.2
-console.log(edd)
+console.log("MONICA UCAK");
 
 
-let konutKredileri=["KGK KGK","WWW WWW","SSS SSS","MMM MMM"]
+var dolarBugun=9.30
 
-for(let i=0;i<konutKredileri.length;i++){
-    console.log(konutKredileri[i])
-}
+console.log(dolarDun)
+var dolarDun=9.20
 
-console.log(konutKredileri)
 
-console.log("<ul>")
-for(let i=0;i<konutKredileri.length;i++){
-    console.log("<li>"+konutKredileri [i]+"</li>")
-}
 
-console.log("</ul>")
---
+++ src/bbp.js
@@ -0,0 +1,30 @@
+console.log("MONICA UCAK")
+//js typesafe degildir
+
+
+let dolarBugun=9.30
+let dolarDun=9.20
+
+
+{
+
+    let dolarDun=9.10
+   
+}
+console.log(dolarDun)
+
+const euroDun=11.2
+
+
+console.log(euroDun)
+
+let konutKredileri=["Konut Kredisi","Emlak Konut kredisi","Kamu Konut Kredisi","Özel Konut Kredisi"]
+console.log(konutKredileri)
+
+for (const kredi of konutKredileri) {
+    console.log(kredi)
+}
+
+for(let i=0; i<konutKredileri.length;i++){
+console.log("<li>"+konutKredileri[i]+"</li>")
+}
